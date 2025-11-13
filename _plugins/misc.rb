9fbdc40c643f15d42713b875df066df4d70d1f57
wxd <972556119@qq.com>
2025-11-13 21:10:33 +0800

    init

--- _plugins/misc.rb
+++ _plugins/misc.rb
@@ -1,5 +1,4 @@
 require 'liquid'
-require 'html-proofer'
 
 module Jekyll
   module MiscFilters
@@ -15,56 +14,26 @@ module Jekyll
       elsif object.is_a?(Array)
         return object
       end
-      return object
+      return []
     end
 
- 
-    def empty_binding
-      binding
-    end
-
-    # make arbitrary string into valid ruby variable name
-    def safe_var_name(name)
-      return name.to_s.gsub(/[^a-z]+/i, "_").gsub(/^_|_$/, "")
-    end
-
-    # filter a list of hashes
-    def data_filter(data, filter)
-      if not filter.is_a?(String)
+    # filter a list of hashes by comma-sep'd field:value pairs
+    def data_filter(data, filters)
+      if not data.is_a?(Array) or not filters.is_a?(String)
         return data
       end
-
-      # filter data
-      return data.clone.select{
-        |item|
-        # if jekyll doc collection, get hash of doc data
-        if item.is_a? Jekyll::Document
-          item = item.data
+      data = data.clone
+      for filter in array_filter(filters.split(","))
+        key, value = array_filter(filter.split(":"))
+        # find unspecified fields
+        if value == nil
+          data.select!{|d| d[key] == nil}
+        # find fields that match regex
+        elsif value.is_a?(String)
+          data.select!{|d| d[key].to_s =~ /#{value}/m}
         end
-        # start with empty context of local variables
-        b = empty_binding
-        # add item as local variable
-        b.local_variable_set("item", item)
-        # also set each item field as local variable when evaluating filter
-        item.each do |var, val|
-          b.local_variable_set(safe_var_name(var), val)
-        end
-        # whether to keep item
-        keep = true
-        while true
-          begin
-            # evaluate expression as true/false
-            keep = !!eval(filter, b)
-            break
-          # if a var in expression isn't a field on item
-          rescue NameError => e
-            # define it and re-evaluate
-            b.local_variable_set(safe_var_name(e.name), nil)
-          end
-        end
-        # keep/discard item
-        keep
-      }
+      end
+      return data
     end
 
     # from css text, find font family definitions and construct google font url
@@ -83,33 +52,6 @@ module Jekyll
         url.delete_suffix!(";")
       end
       return url
-    end
-  end
-
-  # based on https://github.com/episource/jekyll-html-proofer
-  module HtmlProofer
-    priority = Jekyll::Hooks::PRIORITY_MAP[:high] + 1000
-
-    Jekyll::Hooks.register(:site, :post_write, priority: priority) do |site|
-      if not site.config["proofer"] == false
-        options = {
-          allow_missing_href: true,
-          enforce_https: false,
-          ignore_files: [/.*testbed.html/],
-          ignore_urls: [
-            /fonts\.gstatic\.com/,
-            /localhost:/,
-            /0\.0\.0\.0:/,
-          ],
-        }
-
-        begin
-          HTMLProofer.check_directory(site.dest, options).run
-        rescue Exception => error
-          STDERR.puts error
-          # raise error
-        end
-      end
     end
   end
 end
